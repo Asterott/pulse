61b33ad3f77c3d438722c3784792b39891b5182e
Asterott <asterotten140388@gmail.com>
2022-02-11 23:17:10 +0300

    feat: add carousel + adaptive

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swiper-arrow-right',
+    prevEl: '.swiper-arrow-left',
+  },
+  mousewheel: {
+    eventsTarget: '.swiper-wrapper',
+    sensitivity: 1,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  speed: 800,
+  loop: true,
+  // autoHeight: true,
+});
